9fa895c984f34e6bceb74660fcf86480cc41c3fe
megan <meganambermcbride@gmail.com>
2022-06-17 15:21:55 -0400

    Test for helper functions

---
+++ test/helpersTest.js
@@ -0,0 +1,28 @@
+const { assert } = require('chai');
+
+const { getUser } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUser("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert.deepEqual(user.id, expectedUserID)
+  });
+  it('should return false with an invalid email', function() {
+    const user = getUser("user12@example.com", testUsers)
+    assert.isFalse(user)
+  });
+});
